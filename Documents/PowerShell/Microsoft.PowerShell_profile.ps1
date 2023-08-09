64c13622240b65fdfe767c3b3c6c22d810d70dd3
Xavier Beheydt <xavier.beheydt@gmail.com>
2023-08-09 09:35:49 +0200

    Add DockerCompletion module and testing if module is present

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,19 @@
  # Author ; Xavier Beheydt <xavier.beheydt@gmail.com>
 #>
 
+# TODO Make a list for Modules
+
+# TODO Make a helpers
+if (-Not $(Get-Module -ListAvailable -Name posh-git)){
+    Install-Module -Force -Scope CurrentUser posh-git
+}
 Import-Module posh-git
+
+# TODO Make a helpers
+if ( -Not $(Get-Module -ListAvailable -Name DockerCompletion)){
+    Install-Module -Force -Scope CurrentUser DockerCompletion
+}
+Import-Module DockerCompletion
 
 # Import Helpers
 Get-ChildItem "$(Split-Path -Parent $MyInvocation.MyCommand.Path)/Helpers" -Filter *.ps1 |
