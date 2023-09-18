69c046102bd99ed5f22bcfc6c492b1c843c715f2
Xavier Beheydt <xavier.beheydt@gmail.com>
2023-09-18 13:26:57 +0200

    Add ps2exe as module

--- Documents/PowerShell/Microsoft.VSCode_profile.ps1
+++ Documents/PowerShell/Microsoft.VSCode_profile.ps1
@@ -18,6 +18,11 @@ if ( -Not $(Get-Module -ListAvailable -Name DockerCompletion)){
 }
 Import-Module DockerCompletion
 
+if (-Not $(Get-Module -ListAvailable -Name ps2exe)){
+    Install-Module -Force -Scope CurrentUser ps2exe
+}
+Import-Module ps2exe
+
 # Import Helpers
 Get-ChildItem "$(Split-Path -Parent $MyInvocation.MyCommand.Path)/Helpers" -Filter *.ps1 |
 ForEach-Object {
