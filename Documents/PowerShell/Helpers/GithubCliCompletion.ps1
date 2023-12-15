5d6ccbfa812d9b61869cf4e176ca7e2a75c841ca
Xavier Beheydt <xavier.beheydt@gmail.com>
2023-12-15 10:01:21 +0100

    Add a function to open github cli manual in browser

--- Documents/PowerShell/Helpers/GithubCliCompletion.ps1
+++ Documents/PowerShell/Helpers/GithubCliCompletion.ps1
@@ -7,3 +7,7 @@
  if (Get-Command gh -ErrorAction 'SilentlyContinue') {
     Invoke-Expression -Command $(gh completion -s powershell | Out-String)
 }
+
+function Github-Help {
+    Start-Process "https://cli.github.com/manual/"
+}
